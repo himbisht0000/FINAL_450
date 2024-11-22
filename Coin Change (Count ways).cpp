da608c4da19c9e28be48136dd3dd3d56b0a877b1
Himanshu SIngh Bisht <38493189+himbisht0000@users.noreply.github.com>
2024-11-22 20:09:15 +0530

    Update Coin Change (Count ways).cpp

--- Coin Change (Count ways).cpp
+++ Coin Change (Count ways).cpp
@@ -30,3 +30,36 @@ int count(vector<int>& coins, int sum)
         return output;
 }
 
+----------------------------------------DP--BASED--------------------------------------
+ int count(vector<int>& coins, int sum) {
+        int N = coins.size();
+        int M[N+1][sum+1];
+        memset(M , 0, sizeof(M));
+        //M[i][j] represents the number of ways to get sum j when coins are conisder from index i
+        for(int i = 0 ; i<=sum ; i++)
+        {
+            M[N][i] = 0;
+        }
+        
+        for(int i = 0 ; i<=N ; i++)
+        {
+            M[i][0] = 1;
+        }
+        
+        for(int i = N-1 ; i>=0; i--)
+        {
+            for(int j = 1; j<=sum ; j++)
+            {
+                if(j-coins[i] >= 0)
+                {
+                    M[i][j] = M[i][j-coins[i]];
+                }
+                
+                M[i][j]+=M[i+1][j];
+            }
+        }
+        
+        return M[0][sum];
+        
+    }
+
